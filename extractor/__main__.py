4a0f6e91419a40bde56324e67e7b0cb498af5621
no2am0022 <145053398+no2am0022@users.noreply.github.com>
2024-06-13 22:49:30 -0400

    Update __main__.py

--- extractor/__main__.py
+++ extractor/__main__.py
@@ -8,8 +8,8 @@ from extractor.beneficiaries import main as beneficiaries
 base_path = pathlib.Path("./data")
 output_path = pathlib.Path("./output")
 
-# organizations.parse(base_path=base_path, output_file=output_path.joinpath("organizations.csv"))
-# accountants.parse(base_path=base_path, output_file=output_path.joinpath("accountants.csv"))
-# staff.parse(base_path=base_path, output_file=output_path.joinpath("staff.csv"))
+organizations.parse(base_path=base_path, output_file=output_path.joinpath("organizations.csv"))
+accountants.parse(base_path=base_path, output_file=output_path.joinpath("accountants.csv"))
+staff.parse(base_path=base_path, output_file=output_path.joinpath("staff.csv"))
 beneficiaries.parse(base_path=base_path, output_file=output_path.joinpath("beneficiaries.csv"))
 
